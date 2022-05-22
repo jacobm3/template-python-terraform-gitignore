c50bb27c6109ef87c321e112bf3da80cd9b199ba
Jacob <jacobm3@gmail.com>
2022-05-22 03:43:55 -0500

    Update remote.tf

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jm3-state"
-    key    = "project-name"
+    key    = "project-name.tfstate"
     region = "us-east-1"
     dynamodb_table  = "jm3-state"
   }
